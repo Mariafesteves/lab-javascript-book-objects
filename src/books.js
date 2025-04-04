14089ce8a21d863b818c59d61ed61f7741f5d5d7
Mariaesteves <mariaferraesteves@hotmail.com>
2025-04-04 14:58:39 +0100

    solved lab book-objects

--- src/books.js
+++ src/books.js
@@ -9,6 +9,7 @@
 //    description: One of Hemingway's most famous works, it tells the story of Santiago...
 // }
 
+
 // Book 2
 // title: The Airbnb Story
 // pages: 256
@@ -38,27 +39,43 @@
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
+  {title: 'The Old Man and the Sea', pages: 128, author: 'Ernest Hemingway', details: {language: 'English', description: 'One of Hemingway\'s most famous works, it tells the story of Santiago...'}},
+  {title: 'The Airbnb Story', pages: 256, author: 'Leight Gallagher', details: {language: 'English', description: 'This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...'}},
+  {title: 'Educated - A Memoir', pages: 352, author: 'Tara Westover', details: {language: 'English', description: 'Educated is an account of the struggle for self-invention...'}},
+  {title: 'The Art of Learning', pages: 288, author: 'Josh Waitzkin', details: {language: 'English', description: 'The Art of Learning takes readers through Waitzkin\'s unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.'}
 
-
+  }
+];
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
-
-}
-
-
+function getBookDetails(book) {
+  
+  return `${book.title} - ${book.author} - ${book.pages} pages`;
+  }
+// Your code here:
+console.log(getBookDetails(booksArray[3]));
 
 // Iteration 3 | Delete Language
 // Your code here:
+delete booksArray[0].details.language;
+delete booksArray[1].details.language;
+delete booksArray[2].details.language;
+delete booksArray[3].details.language;
+
+console.log(booksArray[0].details.language);
 
 
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+booksArray[0].readingTime = Math.ceil(booksArray[0].pages * 500 / 90);
+booksArray[1].readingTime = Math.ceil(booksArray[1].pages * 500 / 90);
+booksArray[2].readingTime = Math.ceil(booksArray[2].pages * 500 / 90);
+booksArray[3].readingTime = Math.ceil(booksArray[3].pages * 500 / 90);
+console.log(booksArray[0].readingTime);
 
 
 
@@ -86,15 +103,30 @@ const dictionary = {
     ],
 };
 
-function booksByAuthor() {
+function booksByAuthor(dictionary) {
   // Your code here:
-  
+  const finalDictionary = [];
+  for (const author in dictionary) {;
+    for (const book of dictionary[author]) {
+     finalDictionary.push({
+        title: book[0],
+        pages: book[1],
+        author: author,
+      });
+    }
+  }
+  return finalDictionary;
 }
-
+console.log(booksByAuthor(dictionary));
 
 
 // Bonus: Iteration 6 | Average Page Count
-function averagePageCount() {
+function averagePageCount(pages) {
   // Your code here:
-  
+  let totalPages = 0;
+  pages.forEach(function (book) {
+    totalPages += book.pages;
+  })
+ 
+  return totalPages / pages.length;
 }
